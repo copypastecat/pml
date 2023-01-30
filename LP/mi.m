9cdeddb130f16b8cdfe73ad293e3807c55154d90
Leonhard Grosse <leonhardgrosse@gmx.de>
2023-01-30 16:07:15 +0100

    added RR comparison

--- LP/mi.m
+++ LP/mi.m
@@ -4,6 +4,7 @@ function mi = mi(p_ycondx,p_x)
     p_xy = (p_x.*p_ycondx')';
     p_y = sum(p_xy,1);
     logterms = log(p_ycondx./p_y);
+    logterms(isinf(logterms)) = 0;
     t = sum(p_xy.*logterms);
     t(isnan(t))=0;
     mi = sum(t);
